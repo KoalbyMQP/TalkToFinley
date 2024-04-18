7457594fd291867588cb1e4e3daf5b34d68372c1
shaunmcbean <gshivank7@gmail.com>
2024-04-17 20:59:55 -0400

    using switch state

--- electromagnet.py
+++ electromagnet.py
@@ -3,16 +3,25 @@ import RPi.GPIO as GPIO
 import time
 import signal
 
-pin = 17  #change pin
+pin = 17  # change pin
+previous_state = False  # global variable to store previous state
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin, GPIO.OUT)
-def main():
-    signal.signal(signal.SIGINT, onExit)
-    GPIO.output(pin, False)
-    time.sleep(10)
-    GPIO.output(pin, True)
 
-def onExit(self, sig, frame):
+def main():
+    global previous_state  # use the global variable
+
+    signal.signal(signal.SIGINT, onExit)
+
+    # Toggle the state based on the previous state
+    if previous_state:
+        GPIO.output(pin, False)
+        previous_state = False
+    else:
+        GPIO.output(pin, True)
+        previous_state = True
+
+def onExit(sig, frame):  # Remove self parameter
     GPIO.output(pin, True)
     exit(0)
